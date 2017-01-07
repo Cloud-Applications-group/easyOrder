354d3649d1d2c208cb58ab24184071bd4d90e788
astound <soak@live.co.uk>
2017-01-07 01:11:08 +0000

    added menu creation page and made restaurant page read menu from database not from the hardcoded template

--- interface/views.py
+++ interface/views.py
@@ -16,6 +16,10 @@ from django.shortcuts import redirect, render
 import json
 from .models import Restaurant, Menu
 from utils import google_place_details
+import jsonschema
+from jsonschema import validate
+from jsonschema import FormatChecker
+from django.core.exceptions import ObjectDoesNotExist
 
 
 @csrf_protect
@@ -61,89 +65,6 @@ def login(request):
 
 @login_required
 def homepage(request):
-    menu = {
-        "menu": [{
-            "category": [{
-                "title": "Sides",
-                "description": "a little something to accompany your meal",
-                "items": [
-                    {
-                        "item_title": "Chips",
-                        "item_description": "Our signature triple-fried chips",
-                        "item_options": [
-                            {"option_name": "Regular", "price": 1.50}
-                        ]
-                    }, {
-
-                        "item_title": "Chicken Wings",
-                        "item_description": "Piri-piri seasoned wings",
-                        "item_options": [
-                            {"option_name": "Medium", "price": 2.50},
-                            {"option_name": "Hot", "price": 2.50},
-                            {"option_name": "Super Hot", "price": 2.50}
-                        ]
-                    }, {
-
-                        "item_title": "Onion rings",
-                        "item_description": "Beer-battered onion rings",
-                        "item_options": [
-                            {"option_name": "Regular", "price": 1.50}
-                        ]
-                    }
-                ]}, {
-                "title": "Mains",
-                "description": "Big flavour, diner style food",
-                "items": [
-                    {
-                        "item_title": "Burger",
-                        "item_description": "A quarter pounder burger served in a brioche bun with a side of chips or wedges",
-                        "item_options": [
-                            {"option_name": "Beef", "price": 6.50},
-                            {"option_name": "Vegetarian", "price": 5.50}
-                        ]
-                    }, {
-                        "item_title": "Hotdog",
-                        "item_description": "A 9 inch American-style hotdog served with a side of chips or wedges",
-                        "item_options": [
-                            {"option_name": "Hotdog", "price": 5.50}
-                        ]
-                    }, {
-                        "item_title": "Pizza",
-                        "item_description": "Fully loaded deep-pan pizza",
-                        "item_options": [
-                            {"option_name": "Pepperoni", "price": 6.00},
-                            {"option_name": "BBQ Chicken", "price": 6.00},
-                            {"option_name": "Margherita", "price": 4.50}
-                        ]
-                    }
-                ]}, {
-                "title": "Drinks",
-                "description": "",
-                "items": [
-                    {
-                        "item_title": "Coke",
-                        "item_description": "Coca-cola on draught",
-                        "item_options": [
-                            {"option_name": "Regular", "price": 1.00},
-                            {"option_name": "Diet", "price": 1.00}
-                        ]
-                    }, {
-                        "item_title": "Lemonade",
-                        "item_description": "",
-                        "item_options": [
-                            {"option_name": "Regular", "price": 5.50}
-                        ]
-                    }, {
-                        "item_title": "Beer",
-                        "item_description": "400cl glass of Peroni Nastro Azzurro",
-                        "item_options": [
-                            {"option_name": "Peroni", "price": 6.00}
-                        ]
-                    }
-                ]
-            }]
-        }]
-    }
 
     location_id = request.GET.get('place_id')
 
@@ -154,10 +75,62 @@ def homepage(request):
             'error': 'We are not currently supporting this location'}
         return render(request, 'login.html', variables)
 
+    restaurant = Restaurant.objects.all().filter(location_id=location_id)[0]
+    menu2 = Menu.objects.all().filter(restaurant=restaurant)[0].content
+    menu2 = json.loads(menu2.decode('string-escape').strip('"'))
+
+    schema = {
+      "type": "object",
+      "properties": {
+        "menu": {
+          "type": "array",
+          "items": {
+            "type": "object",
+            "properties": {
+              "category": {
+                "type": "array",
+                "items": {
+                  "type": "object",
+                  "properties": {
+                    "title": {"type": "string"},
+                    "description": {"type": "string"},
+                    "items": {
+                      "type": "array",
+                      "items": {
+                        "type" : "object",
+                        "properties": {
+                            "item_title": {"type": "string"},
+                            "item_description": {"type": "string"},
+                            "item_options": {
+                                "type": "array",
+                                "items": {
+                                    "type": "object",
+                                    "properties": {
+                                        "option_name": {"type": "string"},
+                                        "price": {"type": "number"}
+                                    }, "required": ["option_name", "price"]
+                                }
+                            }
+                        },
+                        "required": ["item_title", "item_description", "item_options"]
+                      }
+                    }
+                  },
+                  "required": ["title", "description", "items"]
+                }
+              }
+            },
+            "required": ["category"]
+          }
+        }
+      },
+      "required": ["menu"]
+    }
+
 
     google_place_data = google_place_details(location_id)
 
-    context = {'menu': menu, 'google_place_data' : google_place_data }
+    context = {'menu': menu2, 'google_place_data' : google_place_data }
     return render(request, 'homepage.html', context)
 
 
@@ -188,13 +161,22 @@ def register(request):
 @csrf_protect
 def menu(request):
     if request.method == "POST":
-        menu = Menu.objects.create(
-            user = request.user,
-            restaurant = Restaurant.objects.filter(user=request.user)[:1].get(),
-            content = json.loads(request.body)
-        )
-        menu.save()
-        return HttpResponseRedirect('/')
+        print json.dumps(request.body)
+        restaurant = Restaurant.objects.filter(user=request.user)[:1].get()
+        try:
+            menu = Menu.objects.all().filter(restaurant=restaurant)[:1].get()
+            menu.content = json.dumps(request.body)
+            menu.save()
+            print "updated"
+        except ObjectDoesNotExist:
+            menu = Menu.objects.create(
+                user = request.user,
+                restaurant = restaurant,
+                content = json.dumps(request.body)
+            )
+            menu.save()
+            print "created"
+        return render_to_response('homepage.html')
     else:
         return render_to_response('menu.html')
 
