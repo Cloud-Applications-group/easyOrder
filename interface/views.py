3a555fecd9dd2b5e0eda05ba1431abd016116e23
jackmw94 <jackmw94@gmail.com>
2017-01-07 21:56:42 +0000

    Updated menu template to work with new json, views now loads updated json

--- interface/views.py
+++ interface/views.py
@@ -58,88 +58,72 @@ def login(request):
 
 def place(request):
     menu = {
-        "menu": [{
-            "category": [{
-                "title": "Sides",
-                "description": "a little something to accompany your meal",
-                "items": [
-                    {
-                        "item_title": "Chips",
-                        "item_description": "Our signature triple-fried chips",
-                        "item_options": [
-                            {"option_name": "Regular", "price": 1.50}
-                        ]
-                    }, {
-
-                        "item_title": "Chicken Wings",
-                        "item_description": "Piri-piri seasoned wings",
-                        "item_options": [
-                            {"option_name": "Medium", "price": 2.50},
-                            {"option_name": "Hot", "price": 2.50},
-                            {"option_name": "Super Hot", "price": 2.50}
-                        ]
-                    }, {
-
-                        "item_title": "Onion rings",
-                        "item_description": "Beer-battered onion rings",
-                        "item_options": [
-                            {"option_name": "Regular", "price": 1.50}
-                        ]
-                    }
-                ]}, {
-                "title": "Mains",
-                "description": "Big flavour, diner style food",
-                "items": [
-                    {
-                        "item_title": "Burger",
-                        "item_description": "A quarter pounder burger served in a brioche bun with a side of chips or wedges",
-                        "item_options": [
-                            {"option_name": "Beef", "price": 6.50},
-                            {"option_name": "Vegetarian", "price": 5.50}
-                        ]
-                    }, {
-                        "item_title": "Hotdog",
-                        "item_description": "A 9 inch American-style hotdog served with a side of chips or wedges",
-                        "item_options": [
-                            {"option_name": "Hotdog", "price": 5.50}
-                        ]
-                    }, {
-                        "item_title": "Pizza",
-                        "item_description": "Fully loaded deep-pan pizza",
-                        "item_options": [
-                            {"option_name": "Pepperoni", "price": 6.00},
-                            {"option_name": "BBQ Chicken", "price": 6.00},
-                            {"option_name": "Margherita", "price": 4.50}
-                        ]
-                    }
-                ]}, {
-                "title": "Drinks",
-                "description": "",
-                "items": [
-                    {
-                        "item_title": "Coke",
-                        "item_description": "Coca-cola on draught",
-                        "item_options": [
-                            {"option_name": "Regular", "price": 1.00},
-                            {"option_name": "Diet", "price": 1.00}
-                        ]
-                    }, {
-                        "item_title": "Lemonade",
-                        "item_description": "",
-                        "item_options": [
-                            {"option_name": "Regular", "price": 5.50}
-                        ]
-                    }, {
-                        "item_title": "Beer",
-                        "item_description": "400cl glass of Peroni Nastro Azzurro",
-                        "item_options": [
-                            {"option_name": "Peroni", "price": 6.00}
-                        ]
-                    }
-                ]
-            }]
-        }]
-    }
+		"menu": [{
+			"item_count": 9,  
+			"category": [{
+				"title": "Sides",
+				"category_id": 1,
+				"items": [
+					{
+						"item_id": 1,
+						"item_title": "Chips",
+						"item_description": "Our signature triple-fried chips",
+						"item_price": 1.50
+					}, {
+						"item_id": 2,
+						"item_title": "Chicken Wings",
+						"item_description": "Piri-piri seasoned wings",
+						"item_price": 2.50
+					}, {
+						"item_id": 3,
+						"item_title": "Onion rings",
+						"item_description": "Beer-battered onion rings",
+						"item_price": 2.00
+					}
+				]}, {
+				"title": "Mains",
+				"category_id": 2,
+				"items": [
+					{
+						"item_id": 1,
+						"item_title": "Hotdog",
+						"item_description": "A 9 inch American-style hotdog served with a side of chips or wedges",
+						"item_price": 5.50
+					}, {
+						"item_id": 2,
+						"item_title": "Pizza",
+						"item_description": "Fully loaded deep-pan pepperoni pizza",
+						"item_price": 7.50
+					}, {
+						"item_id": 3,
+						"item_title": "Burger",
+						"item_description": "100% beef quarter pounder, served in a brioche bun with a side of chips or wedges",
+						"item_price": 6.50
+					}
+				]}, {
+				"title": "Drinks",
+				"category_id": 3,
+				"items": [
+					{
+						"item_id": 1,
+						"item_title": "Coke",
+						"item_description": "Coca-cola on draught",
+						"item_price": 1.00
+					}, {
+						"item_id": 2,
+						"item_title": "Lemonade",
+						"item_description": "",
+						"item_price": 5.50
+					}, {
+						"item_id": 3,
+						"item_title": "Beer",
+						"item_description": "400cl glass of Peroni Nastro Azzurro",
+						"item_price": 6.00
+					}]
+				}]
+			}
+		]
+	}
 
     user = request.user
     context = {}
