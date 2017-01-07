5a1f36fc137946ac2af630fa952505d2137a601b
Dennisparchkov <dp1u12@soton.ac.uk>
2017-01-07 12:23:37 +0000

    update model with restaurant availability, popularity, order resrvation time

--- interface/models.py
+++ interface/models.py
@@ -10,15 +10,32 @@ from django.conf import settings
 STATUS_NEW              = 0
 STATUS_PROCESSING       = 1
 STATUS_FAILED           = 2
-STATUS_COMPLETED        = 3
+STATUS_READY            = 3
+STATUS_COMPLETED        = 4
 
 
 STATUS_CHOICES = (
     (STATUS_NEW,                'New'),
     (STATUS_PROCESSING,         'Processing'),
     (STATUS_FAILED,             'Failed'),
+    (STATUS_READY,             'Failed'),
     (STATUS_COMPLETED,          'Completed')
 )
+
+#Popularity
+
+STATUS_NOT_BUSY              = 0
+STATUS_BUSY                  = 1
+STATUS_FULL                  = 2
+
+
+
+STATUS_POP_CHOICES = (
+    (STATUS_NOT_BUSY,     'Not Busy'),
+    (STATUS_BUSY,         'Busy'),
+    (STATUS_FULL,         'Full'),
+)
+
 
 def add_now():
     return datetime.now(getattr(pytz, settings.TIME_ZONE))
@@ -28,6 +45,8 @@ class Restaurant(models.Model):
     location_id = models.CharField("Unique Id", null=True, blank=True, default=None, max_length=4096)
     name = models.CharField("Restaurant Name", null=True, blank=True, default=None, max_length=4096)
     info = models.TextField(null=True, blank=True)
+    is_available = models.BooleanField(null=False, blank=False, default=False)
+    popularity = models.IntegerField(null=False, blank=False, default=STATUS_NOT_BUSY, choices=STATUS_POP_CHOICES)
 
     def __unicode__(self):
         return u'%s' % self.name
@@ -40,6 +59,7 @@ class Order(models.Model):
     amount = models.IntegerField(null=False, blank=False)
     status = models.IntegerField(null=False, blank=False, default=STATUS_NEW, choices=STATUS_CHOICES)
     date_time = models.DateTimeField("Submitted on", null=False, blank=True, default=add_now)
+    reservation_date_time = models.DateTimeField("Reserve time", null=False, blank=False, default=add_now)
 
 class Menu(models.Model):
     restaurant = models.ForeignKey(Restaurant, null=False, blank=False)
